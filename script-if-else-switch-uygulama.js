c131bf314d68f4b4b8dacb83454385a69b3cd86f
Kemal Tütüncü <90035121+kemaltt@users.noreply.github.com>
2021-09-17 16:34:45 +0200

    Add files via upload

---
+++ script-if-else-switch-uygulama.js
@@ -0,0 +1,56 @@
+// 20.04.2017 de trafiğeçıkan aracın servis bakım süreleri aşağıda verilmiş.
+//  bu bilgileregöre servis uyarısı versin
+
+// 1. bakım :1 yıl--2. bakım : 2 yıl---3. bakım:3 yıl
+
+// 
+
+//------------------------------------------------------------------------------------------------
+
+var result = prompt("who's there ?");
+
+if (result == 'cancel') {
+
+    console.log('cancelled');
+
+} else if (result == 'Admin') {
+
+    console.log('Welcome Admin');
+
+
+var key = prompt('enter your password');
+
+
+if (key=='cancel' ){
+
+    console.log('cancelled');
+    
+} else if (key=='2134') {
+
+    console.log('Welcome Admin');
+    
+} else {
+    
+    console.log('wrong password');
+    
+}
+  
+{
+
+    
+}
+
+
+
+
+} else {
+    console.log('I dont know');
+
+}
+
+{
+
+}
+
+
+
