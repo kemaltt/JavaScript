aa8f8cdeb51f93b823b96e82d86cedcec44be3db
kemaltt <ttnckemal@gmail.com>
2021-09-18 13:45:10 +0200

    edit3

--- script-if-else-switch-uygulama.js
+++ script-if-else-switch-uygulama.js
@@ -1,10 +1,34 @@
-<<<<<<< HEAD
 // 20.04.2017 de trafiğeçıkan aracın servis bakım süreleri aşağıda verilmiş.
 //  bu bilgileregöre servis uyarısı versin
 
 // 1. bakım :1 yıl--2. bakım : 2 yıl---3. bakım:3 yıl
 
-// 
+let trafigeCık = new Date('04/20/2020');
+
+trafigeCık.setHours(0,0,0,0);
+let trafikMs = Date.now() - trafigeCık.getTime();
+
+let trafikGun =Math.floor( trafikMs/(1000*60*60*24));
+
+
+if (trafikGun<=365) {
+    
+    console.log('1.servis gününüz geldi');
+
+} else if (trafikGun>365 && trafikGun<=365*2) {
+    console.log('2.servis gününüz geldi');
+} else if (trafikGun>365*2 && trafikGun<=365*3) {
+    console.log('3.servis gününüz geldi')
+
+} else {
+    console.log('bilinmeyen değer');
+    
+}{
+    
+}{
+    
+}
+console.log(trafikGun);
 
 //------------------------------------------------------------------------------------------------
 
@@ -19,83 +43,26 @@ if (result == 'cancel') {
     console.log('Welcome Admin');
 
 
-var key = prompt('enter your password');
+    var key = prompt('enter your password');
 
 
-if (key=='cancel' ){
+    if (key == 'cancel') {
 
-    console.log('cancelled');
-    
-} else if (key=='2135') {
+        console.log('cancelled');
 
-    console.log('Welcome Admin');
-    
-} else {
-    
-    console.log('wrong password');
-    
-}
-  
-{
+    } else if (key == '2135') {
 
-}
+        console.log('Welcome Admin');
 
+    } else {
 
-} else {
-    console.log('I dont know');
+        console.log('wrong password');
 
-}
+    }
 
-{
+    {
 
-}
-
-
-
-=======
-// 20.04.2017 de trafiğeçıkan aracın servis bakım süreleri aşağıda verilmiş.
-//  bu bilgileregöre servis uyarısı versin
-
-// 1. bakım :1 yıl--2. bakım : 2 yıl---3. bakım:3 yıl
-
-// 
-
-//------------------------------------------------------------------------------------------------
-
-var result = prompt("who's there ?");
-
-if (result == 'cancel') {
-
-    console.log('cancelled');
-
-} else if (result == 'Admin') {
-
-    console.log('Welcome Admin');
-
-
-var key = prompt('enter your password');
-
-
-if (key=='cancel' ){
-
-    console.log('cancelled');
-    
-} else if (key=='2134') {
-
-    console.log('Welcome Admin');
-    
-} else {
-    
-    console.log('wrong password');
-    
-}
-  
-{
-
-    
-}
-
-
+    }
 
 
 } else {
@@ -109,4 +76,4 @@ if (key=='cancel' ){
 
 
 
->>>>>>> c131bf314d68f4b4b8dacb83454385a69b3cd86f
+
