4f014b9297a538820555388b7af52f35e859a3b3
kemaltt <ttnckemal@gmail.com>
2021-09-28 12:29:20 +0200

    update

---
+++ window-object.js
@@ -0,0 +1,69 @@
+// Window Object
+
+let val;
+var a = 10;
+function abc() {
+  return 0;
+}
+
+val = window;
+
+
+// // alert-------------------------------------------------------
+
+ alert('Merhaba');
+
+
+// // prompt-----------------------------------------------------
+
+ var b = prompt('bir sayı giriniz');
+
+ console.log(b);
+
+// // confirm--------------------------------------------------------
+
+ var c = confirm('emin misiniz?') // true yada false döner
+
+ if (c) {
+     console.log('ok');
+    
+ }else{
+     console.log('no');
+ }
+
+ console.log(c);
+
+// scroll----------------------------------------------------------------
+
+ window.scrollX;
+
+var d = scrollX;
+var d = scrollY;
+
+ console.log(d);
+
+// Location----------------------------------------------------
+
+
+val = window.location;
+val = window.location.href;
+val = window.location.host;
+val = window.location.protocol; 
+
+//val = window.location.href='http://senattnc.com';
+
+//window.location.reload(); sayfa yenilemek için
+
+// navigator--------------------------------------------------------------
+
+
+
+
+
+
+
+
+console.log(val);
+
+
+
